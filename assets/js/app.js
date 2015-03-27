205737c2ac886e0f650f85ffc27d5014d55d5802
Daniel Matthew <hello@danmatthew.co.uk>
2015-03-27 15:46:02 +0000

    Keyboard navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,91 @@ $(document).ready(function(){
       principle1 = $('#principle-1'),
       text;
 
-  // principle1.on('click', function(e){
-  //   enlarged.text($(this).children('.summary').text());
-  // });
+  // Bind cursor keys, WASD, and swipe events to
+  // enable navigation through chapters and principles
+
+    var splashScreen = $('#splash');
+
+
+  function hideSplashScreen() {
+    var splashScreen = document.getElementById('splash');
+    splashScreen.classList.add('fadeOut');
+  }
+
+  var counter = 0;
+  console.log('Counter is: ' + counter);
+
+  function navigateRight() {
+
+    if (counter < fundamentals.length - 1) {
+      counter++;
+      console.log('Counter is ' + counter);
+    } else {
+      console.log('end of array');
+    }
+
+    return counter;
+  }
+
+  function navigateLeft() {
+    if (counter >= 1) {
+      counter--;
+    } else {
+      console.log('start of array');
+      // Maybe wrap round? 
+    }
+  }
+
+  function updateText(chapter) {
+    title.text($(fundamentals[chapter]).data('lesson'));
+    quote.text($(fundamentals[chapter]).data('quote'));
+  }
+
+  // Get all chapters in fundamentals section
+  var fundamentals = $('#fundamentals article');
+  console.log('Fundamentals length is ' + fundamentals.length);
+
+
+
+  $(document).keydown(function(e) {
+    switch (e.which) {
+      case 32: // Spacebar - hide menu screen
+        hideSplashScreen();
+        break;
+      case 37: //left
+        navigateLeft();
+        // counter--;
+        updateText(counter);
+        console.log('Pressed left arrow'); 
+        break;
+
+      case 38: // up
+        console.log('Pressed up arrow');
+        break;
+
+      case 39: // right
+        // title.text($(fundamentals))
+        navigateRight();
+        // counter++;
+        // navigateRight(counter);
+        updateText(counter);
+        console.log(counter);
+        console.log('Pressed right arrow');
+        break;
+
+      case 40: // down
+        console.log('Pressed down arrow');
+        break;
+
+      default: 
+        break;
+    }
+    e.preventDefault();
+  });
+
+
+  updateText(counter);
+
 
 
   articles.on('click', function(){
